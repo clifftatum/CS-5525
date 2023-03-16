231cc3195fccc36905ef839afc8bb12f268c7bd3
clifftatum <cliffordt@vt.edu>
2023-03-16 10:47:26 -0400

    dev commit, reorg folder structures, added .pdf

--- tem_proj.py
+++ tem_proj.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-from EDA import EDA
+from src.EDA import EDA
 import seaborn as sb
 import plotly.express as px
 import numpy as np
@@ -13,16 +13,25 @@ if __name__ == '__main__':
     eda = EDA('Clifford Tatum EDA Deflation Market Nov 2022 - Jan 2023 - CS-5525')
     url = 'https://raw.githubusercontent.com/clifftatum/CS-5525-Term-Project/main/DELIVER_CS5525_term_dataset_cliffordt_Deflation_EcoEnv_Nov2022_Jan2023.csv'
     df = pd.read_csv(url)
+
+    # Change the date feature to a datetime
     df['Date'] = pd.to_datetime(df['Date'])
+
+    # Sort the date by oldest to newest
     df.sort_values(by='Date', inplace=True,ascending = True)
 
-    eda.get_percent_of_missing_observations(df,clean=False,show=True,clean_method=None)
+    # Prune the useless Features
+    df.drop(columns='Unnamed: 0',inplace=True)
+
+    num_missing, percent_removed, df_clean = eda.get_percent_of_missing_observations(df,clean=True,
+                                                                                        show=True,
+                                                                                        clean_method='prune')
 
 
-    fig = px.scatter(df, x="SlowPctK", y="PctB", color="target_buy_sell_performance", marginal_y="violin",
-                     marginal_x="box", trendline="ols", template="simple_white")
-    fig.show()
+    # fig = px.scatter(df, x="SlowPctK", y="PctB", color="target_buy_sell_performance", marginal_y="violin",
+    #                  marginal_x="box", trendline="ols", template="simple_white")
+    # fig.show()
 
-    df.drop(['price_pct_change'],axis1,inplace=True)
+    # df.drop(['price_pct_change'],axis1,inplace=True)
 
     pass
